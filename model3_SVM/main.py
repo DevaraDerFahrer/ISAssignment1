519c62ff5f1dcfe905202e16ba1200e6a18def63
Devara Tertia <devaratertia@gmail.com>
2025-01-02 20:12:45 +0100

    added SVM

--- model3_SVM/main.py
+++ model3_SVM/main.py
@@ -6,16 +6,19 @@ from torchvision.transforms import ToTensor
 from torchvision.transforms import Grayscale
 from torchvision.transforms import Normalize
 import matplotlib.pyplot as plt
+import pickle
 from torch.utils.data import DataLoader
 import torch.nn as tNN
 import torch.optim as tOptim
 import time
 import os
+import sys
 import torchvision.transforms as transforms
 from torchvision import datasets, models
+from sklearn import svm
 from sklearn.svm import SVC
 from sklearn.metrics import classification_report, accuracy_score
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
 import numpy as np
 
 def SavePlotAsVectors(x, y, title, xlabel, ylabel, filename, output_dir="vector_images"):
@@ -47,53 +50,130 @@ def ExtractFeatures(device, model, dataLoader):
             outputs = model.maxPool(outputs)
             outputs = tNN.functional.relu(model.conv4(outputs))
             outputs = model.maxPool(outputs)
-            outputs = outputs.view(outputs.size(0), -1)
+            outputs = outputs.view(-1, model.inputSize)
 
-            features.append(outputs)
-            labels.append(target)
+            features.append(outputs.cpu()[0])
+            labels.append(target.cpu()[0])
             
-    return torch.cat(features), torch.cat(labels)
+    return features, labels
 
-class model3_SVM1(tNN.Module):
-    def __init__(self, inputDimension, numClasses):
-        super(model3_SVM1, self).__init__()
-        self.weights = tNN.Parameter(torch.randn(inputDimension, numClasses))
-        self.bias = tNN.Parameter(torch.zeros(numClasses))
-
-    def forward(self, x):
-        return torch.matmul(x, self.weights) + self.bias
-
-def HingeLossFunction(device, model, labels, inputs, numClasses=10, regularization=0.001):
-    oneHotLabels = torch.nn.functional.one_hot(labels, num_classes=numClasses).float()
-    oneHotLabels = oneHotLabels.to(device)
-    inputs = inputs.to(device)
-    margins = 1 - oneHotLabels * inputs
-    print("oit")
-    hinge = torch.clamp(margins, min=0).sum(dim=1).mean()
-    regTerm = regularization * torch.norm(model.weights) ** 2
-    return hinge + regTerm
-
-
-def training(device, model, labels, features, criterion, optimizer, epoch):
-    model.train()
-    optimizer.zero_grad()
-    outputs = model(features)
-    loss = criterion(device, model, labels, outputs)
-    loss.backward()
-    optimizer.step()
-    print(f"Train epoch: {epoch} \t {loss.item():.6f}")
-
-def testing(device, model, criterion, testLabels, testFeatures):
-    model.eval()
+def preprocessMNIST():
+    inputSize = 32
     
-    with torch.no_grad():
-        outputs = model(testFeatures)
-        predictions = torch.argmax(outputs, dim=1)
-        testAccuracy = (predictions == testLabels).float().mean().item()
-        testLoss = criterion(device, model, testLabels, outputs)
+    trainingTF = torchvision.transforms.Compose([
+        transforms.RandomHorizontalFlip(p=0.5),
+        transforms.Resize(inputSize),
+        transforms.RandomCrop(inputSize, padding=4),
+        transforms.RandomRotation(10),
+        ToTensor(),
+        Normalize((0.5), (0.5))
+        ])
     
-    print(f"\nTest set: Loss: {testLoss.item()*100}, Accuracy {testAccuracy*100}\n")
-    return testLoss.item(), testAccuracy
+    testTF = torchvision.transforms.Compose([
+        transforms.Resize(inputSize),
+        ToTensor(),
+        Normalize((0.5), (0.5))
+        ])
+    
+    trainData = datasets.MNIST(
+        root="../data",
+        train= True,
+        transform=trainingTF,
+        download=True
+    )
+    
+    testData = datasets.MNIST(
+        root="../data",
+        train= False,
+        transform=testTF,
+        download=True
+    )
+    
+    dataLoaders = {
+        'train': DataLoader(
+            trainData,
+            shuffle=True,
+            num_workers=4
+        ),
+        'test': DataLoader(
+            testData,
+            shuffle=True,
+            num_workers=4
+        )
+    }
+    
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    cnnModel = torch.jit.load(f"../model2_CNN1/model2_CNN1_Scripted_mnist.pt").to(device)
+    cnnModel.eval()
+
+    trainFeatures, trainLabels = ExtractFeatures(device, cnnModel, dataLoaders['train'])
+    testFeatures, testLabels = ExtractFeatures(device, cnnModel, dataLoaders['test'])
+    
+    pickle.dump(trainFeatures, open('trainFeatures_mnist.pkl', 'wb'))
+    pickle.dump(trainLabels, open('trainLabels_mnist.pkl', 'wb'))
+    pickle.dump(testFeatures, open('testFeatures_mnist.pkl', 'wb'))
+    pickle.dump(testLabels, open('testLabels_mnist.pkl', 'wb'))
+    
+    return trainFeatures, trainLabels, testFeatures, testLabels
+
+def preprocessCIFAR10():
+    inputSize = 32
+    
+    trainingTF = torchvision.transforms.Compose([
+        transforms.RandomHorizontalFlip(p=0.5),
+        transforms.RandomCrop(inputSize, padding=4),
+        transforms.RandomRotation(10),
+        ToTensor(),
+        Normalize((0.5), (0.5))
+        ])
+    
+    testTF = torchvision.transforms.Compose([
+        ToTensor(),
+        Normalize((0.5), (0.5))
+        ])
+    
+    trainData = datasets.CIFAR10(
+        root="../data",
+        train= True,
+        transform=trainingTF,
+        download=True
+    )
+    
+    testData = datasets.CIFAR10(
+        root="../data",
+        train= False,
+        transform=testTF,
+        download=True
+    )
+    
+    dataLoaders = {
+        'train': DataLoader(
+            trainData,
+            shuffle=True,
+            num_workers=4
+        ),
+        'test': DataLoader(
+            testData,
+            shuffle=True,
+            num_workers=4
+        )
+    }
+    
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    cnnModel = torch.jit.load(f"../model2_CNN1/model2_CNN1_Scripted_cifar10.pt").to(device)
+    cnnModel.eval()
+
+    trainFeatures, trainLabels = ExtractFeatures(device, cnnModel, dataLoaders['train'])
+    testFeatures, testLabels = ExtractFeatures(device, cnnModel, dataLoaders['test'])
+    
+    pickle.dump(trainFeatures, open('trainFeatures_cifar10.pkl', 'wb'))
+    pickle.dump(trainLabels, open('trainLabels_cifar10.pkl', 'wb'))
+    pickle.dump(testFeatures, open('testFeatures_cifar10.pkl', 'wb'))
+    pickle.dump(testLabels, open('testLabels_cifar10.pkl', 'wb'))
+    
+    return trainFeatures, trainLabels, testFeatures, testLabels
 
 def main():
     
@@ -103,143 +183,46 @@ def main():
     while not(datasetName == "mnist" or datasetName == "cifar10" or datasetName == "exit"):
         datasetName = input("Choose dataset[mnist/cifar10]: ")
     
-    trainData = any
-    testData = any
-    
+    trainFeatures = any
+    trainLabels = any
+    testFeatures = any
+    testLabels = any
+   
     if datasetName == "exit":
         return
     elif datasetName == "mnist":
-        inputChannel = 1
-        inputSize = 32
-        numClassess = 10
-        
-        trainingTF = torchvision.transforms.Compose([
-            transforms.RandomHorizontalFlip(p=0.5),
-            transforms.Resize(inputSize),
-            transforms.RandomCrop(inputSize, padding=4),
-            transforms.RandomRotation(10),
-            ToTensor(),
-            Normalize((0.5), (0.5))
-            ])
-        
-        testTF = torchvision.transforms.Compose([
-            transforms.Resize(inputSize),
-            ToTensor(),
-            Normalize((0.5), (0.5))
-            ])
-        
-        trainData = datasets.MNIST(
-            root="../data",
-            train= True,
-            transform=trainingTF,
-            download=True
-        )
-
-        testData = datasets.MNIST(
-            root="../data",
-            train= False,
-            transform=testTF,
-            download=True
-        )
+        try:
+            trainFeatures = pickle.load(open('trainFeatures_mnist.pkl', 'rb'))
+            trainLabels = pickle.load(open('trainLabels_mnist.pkl', 'rb'))
+            testFeatures = pickle.load(open('testFeatures_mnist.pkl', 'rb'))
+            testLabels = pickle.load(open('testLabels_mnist.pkl', 'rb'))
+        except:
+           trainFeatures, trainLabels, testFeatures, testLabels = preprocessMNIST()
         
     elif datasetName == "cifar10":
-        inputChannel = 3
-        inputSize = 32
-        numClassess = 10
-        
-        trainingTF = torchvision.transforms.Compose([
-            transforms.RandomHorizontalFlip(p=0.5),
-            transforms.RandomCrop(inputSize, padding=4),
-            transforms.RandomRotation(10),
-            ToTensor(),
-            Normalize((0.5), (0.5))
-            ])
-        
-        testTF = torchvision.transforms.Compose([
-            ToTensor(),
-            Normalize((0.5), (0.5))
-            ])
-        
-        trainData = datasets.CIFAR10(
-            root="../data",
-            train= True,
-            transform=trainingTF,
-            download=True
-        )
-        
-        testData = datasets.CIFAR10(
-            root="../data",
-            train= False,
-            transform=testTF,
-            download=True
-        )
-
+        try:
+            trainFeatures = pickle.load(open('trainFeatures_cifar10.pkl', 'rb'))
+            trainLabels = pickle.load(open('trainLabels_cifar10.pkl', 'rb'))
+            testFeatures = pickle.load(open('testFeatures_cifar10.pkl', 'rb'))
+            testLabels = pickle.load(open('testLabels_cifar10.pkl', 'rb'))
+        except:
+           trainFeatures, trainLabels, testFeatures, testLabels = preprocessCIFAR10()
+           
     else:
         return
     
-    dataLoaders = {
-        'train': DataLoader(
-            trainData,
-            batch_size=128,
-            shuffle=True,
-            num_workers=4
-        ),
-        'test': DataLoader(
-            testData,
-            batch_size=128,
-            shuffle=True,
-            num_workers=4
-        )
+    param_grid = {
+        'C': [0.1, 1, 10, 100],
+        'kernel': ['linear', 'rbf', 'poly'],
+        'gamma': [0.001, 0.01, 0.1, 1]
     }
-    
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-    cnnModel = torch.jit.load(f"../model2_CNN1/model2_CNN1_Scripted_{datasetName}.pt").to(device)
-    cnnModel.eval()
-
-    trainFeatures, trainLabels = ExtractFeatures(device, cnnModel, dataLoaders['train'])
-    testFeatures, testLabels = ExtractFeatures(device, cnnModel, dataLoaders['test'])
-    
-    trainFeatures = trainFeatures.to(device)
-    trainLabels = trainLabels.to(device)
-    testFeatures = testFeatures.to(device)
-    testLabels = testLabels.to(device)
-
-    inputDimension = trainFeatures.shape[1]
-    model = model3_SVM1(128, numClassess).to(device)
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
-    
-    numOfEpoch = 100
-    
-    losses = []
-    accuracies = []
-
-    for epoch in range(1, numOfEpoch+1):
-        criterion = HingeLossFunction
-        training(device, model, trainLabels, trainFeatures, criterion, optimizer, epoch)
-        testLoss, testAccuracy = testing(device, model, criterion, testFeatures, testLabels)
-        losses.append(testLoss)
-        accuracies.append(testAccuracy)
-        
-    SavePlotAsVectors(
-        x=range(1, numOfEpoch+1), y=losses,
-        title="Training Loss Over Epochs",
-        xlabel="Epochs", ylabel="Loss",
-        filename=f"training_loss_{datasetName}",
-        output_dir="results"
-    )
-
-    SavePlotAsVectors(
-        x=range(1, numOfEpoch+1), y=accuracies,
-        title="Test Accuracy Over Epochs",
-        xlabel="Epochs", ylabel="Accuracy (%)",
-        filename=f"test_accuracy_{datasetName}",
-        output_dir="results"
-    )
-
-    torch.save(model, f'model3_SVM1_{datasetName}.pt')
-    modelScripted = torch.jit.script(model)
-    modelScripted.save(f'model3_SVM1_Scripted_{datasetName}.pt')
+    classifier = GridSearchCV(SVC(), param_grid, cv=5, scoring='accuracy', n_jobs=-1)
+    classifier.fit(trainFeatures, trainLabels)
+    bestModel = classifier.best_estimator_
+    predLabels = bestModel.predict(testFeatures)
+    accuracy = accuracy_score(testLabels, predLabels)
+    print(accuracy)
+    pickle.dump(bestModel, open(f'model3_SVM1_{datasetName}.pkl', 'wb'))
     print(f"model saved, elapsed time: {time.time() - programStartTime}")
 
 if __name__ == '__main__':
